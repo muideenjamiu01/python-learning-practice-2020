c6914715dc70c204f5a0cd5372ea0453bd4103ce
MUIDEEN MUHAMMED JAMIU <Muideenjamiu01@.com>
2021-10-30 15:16:31 +0100

    my python practice 2020

---
+++ student info/studentinformation.py
@@ -0,0 +1,10 @@
+# program to input and print information of a student
+print"what is your name?"
+name = raw_input()
+print"how old are you?"
+age = int(raw_input())
+print"where are you living?"
+address = raw_input()
+print "so your name is %s,you are %s yrs old, and you are living %s" %(name,age,address)
+
+ 
---
+++ text.py
@@ -0,0 +1,4 @@
+# this is my first program
+
+ print("jamiu0")
+
